4a7bae436bd3f56a944c594a095c555f68d069a8
Artyom <artyom.zayarny@gmail.com>
2020-03-22 20:26:01 +0200

    Updates

---
+++ precache-manifest.3180686001c161a602eedc4d97bd3159.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0b23d0520763db5bc6fcc384896f209",
+    "url": "/blog-app/index.html"
+  },
+  {
+    "revision": "c77b2ee9906853d81416",
+    "url": "/blog-app/static/css/main.6940b29e.chunk.css"
+  },
+  {
+    "revision": "1259ca6cab20eda61def",
+    "url": "/blog-app/static/js/2.b3e5e0bd.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/blog-app/static/js/2.b3e5e0bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c77b2ee9906853d81416",
+    "url": "/blog-app/static/js/main.5522c162.chunk.js"
+  },
+  {
+    "revision": "905efe01fb91f44400b0",
+    "url": "/blog-app/static/js/runtime-main.cd64f752.js"
+  }
+]);
